961227eb852fb97b4dcef390a84bc72297a7ee13
Hao Cheng <reworkhow@gmail.com>
2016-09-30 16:23:15 -0400

    refresh structrue

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from flaskexample import app
-#app.run(debug = True)
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000)
+
+app.run(debug = True)
+
+#if __name__=="__main__":
+#    app.run(host='0.0.0.0',port=5000)
